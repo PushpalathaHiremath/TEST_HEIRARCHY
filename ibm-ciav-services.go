89e24c1e69f26e72125a6221693706cc2b8b2111
PushpalathaHiremath <puhirema@in.ibm.com>
2016-08-26 11:40:34 +0530

    Update ibm-ciav-services.go

--- ibm-ciav-services.go
+++ ibm-ciav-services.go
@@ -207,7 +207,7 @@ func (t *ServicesChaincode) Invoke(stub *shim.ChaincodeStub, function string, ar
 func (t *ServicesChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if function == "getCIAV" {
 		return t.getCIAV(stub, args)
-	}else if function == "getCIAV" {
+	}else if function == "getKYCStats" {
 		return t.GetKYCStats(stub)
 	}
 	return nil, errors.New("Received unknown function invocation")
