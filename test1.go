765f808a8b49d2311417f1e8c26bc79f8bce03d3
PushpalathaHiremath <puhirema@in.ibm.com>
2016-09-14 13:07:53 +0530

    Update test1.go

--- test1.go
+++ test1.go
@@ -2,9 +2,11 @@ package ROV_CIAV
 
 import (
 	"fmt"
+	"github.com/op/go-logging"
 )
 
+var myLogger = logging.MustGetLogger("customer_address_details")
 func main() {
-	fmt.Println("Hello, playground")
+	fmt.Println("Hello, My color is : , [%s]", GetColor())
 }
 
