225355517f7273058be1ccdaa807ec917d08ef68
PushpalathaHiremath <puhirema@in.ibm.com>
2016-09-14 13:11:58 +0530

    Update test1.go

--- test1.go
+++ test1.go
@@ -6,8 +6,26 @@ import (
 )
 
 var myLogger = logging.MustGetLogger("Color")
+type ServicesChaincode struct {
+}
 
-func main() {
+func (t *ServicesChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	myLogger.Debug("Hello, My color is : , [%s]", GetColor())
+	return nil, nil
+}
+
+func (t *ServicesChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	return nil, nil
+}
+
+func (t *ServicesChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	return nil, nil
+	
+}
+func main() {
+	err := shim.Start(new(ServicesChaincode))
+	if err != nil {
+		fmt.Printf("Error starting ServicesChaincode: %s", err)
+	}
 }
 
