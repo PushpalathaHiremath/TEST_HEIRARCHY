b130618cfc6acdec536f3dca4c84481da22b43f9
PushpalathaHiremath <puhirema@in.ibm.com>
2016-10-03 17:06:34 +0530

    Update ibm-ciav-services.go

--- ibm-ciav-services.go
+++ ibm-ciav-services.go
@@ -9,7 +9,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	//"github.com/hyperledger/fabric/core/chaincode"
+	"github.com/hyperledger/fabric/core/chaincode"
 	"github.com/op/go-logging"
 	"github.com/hyperledger/fabric/core/chaincode/shim/crypto/attr"
 )
